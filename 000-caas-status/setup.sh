c5f5b643cfc7d897d62c2930801cea5550e6681e
HeL <herve.leclerc@alterway.fr>
2016-10-11 09:21:50 +0200

    add timeout to docker script

--- 000-caas-status/setup.sh
+++ 000-caas-status/setup.sh
@@ -12,6 +12,19 @@ do
   fi
 done
 
+c=0
+until docker ps | grep compose_cachet_1
+do
+  echo "Cachet service is not up"
+  sleep 60
+  let c=${c}+1
+  if [ "${c}" -gt 9 ]; then
+    log "Timeout on Cachet Service after 10 mn"
+    exit 1
+  fi
+done
+
 docker exec -it compose_cachet_1  php artisan key:generate
 docker exec -it compose_cachet_1  php artisan app:install
 docker exec -it compose_cachet_1  php artisan config:cache
+
